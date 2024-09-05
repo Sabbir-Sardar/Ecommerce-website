15ba8b71267da61432cc0ec6a22bc46c3ac76b50
Sabbir-Sardar <personal.smsabbir@gmail.com>
2024-09-05 19:20:17 +0600

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Symfony\Component\HttpKernel\Profiler\Profile;
+
+
+class User extends Model
+{
+    protected $fillable = ['email', 'otp'];
+    public function profile():HasOne
+    {
+        return $this->hasOne(CustomerProfile::class);
+    }
+
+
+}
